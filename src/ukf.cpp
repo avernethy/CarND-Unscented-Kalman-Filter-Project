11b0b86c03111f7d4c771a6421b5b779fcf94693
Vern Francisco <avernethy@gmail.com>
2018-08-23 22:47:09 -0500

    change call to predict

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -94,7 +94,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  }
 
  double delta_t = (meas_package.timestamp_ - previous_time_stamp)/1000000.0;
- Xsig_pred_ = UKF.Prediction(delta_t);
+ Xsig_pred_ = ukf.Prediction(delta_t);
 }
 
 /**
