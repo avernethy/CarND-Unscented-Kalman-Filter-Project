f31c02cb4984c3455a1fc0d8879a5d834baaf945
Vern Francisco <avernethy@gmail.com>
2018-08-23 22:50:08 -0500

    change function call to predict

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -94,7 +94,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
  }
 
  double delta_t = (meas_package.timestamp_ - previous_time_stamp)/1000000.0;
- Xsig_pred_ = ukf.Prediction(delta_t);
+ UKF::Prediction(delta_t);
 }
 
 /**
@@ -164,7 +164,7 @@ void UKF::Prediction(double delta_t) {
       Xsig_aug.col(ii + 1 + n_aug) = x_aug - scale_factor * A.col(ii);
   }
 
-  MatrixXd Xsig_pred = MatrixXd(n_x, 2 * n_aug + 1);
+ Xsig_pred_ = MatrixXd(n_x, 2 * n_aug + 1);
   
   
   //predict sigma points
@@ -206,7 +206,7 @@ void UKF::Prediction(double delta_t) {
       term2(3) = dt2 * nu_psi2;
       term2(4) = delta_t * nu_psi2;
       
-      Xsig_pred.col(ii) = x + term1 + term2;
+      Xsig_pred_.col(ii) = x + term1 + term2;
 
       return Xsig_pred;
        
