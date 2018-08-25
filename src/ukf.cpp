d7187413cf674daa0532ff8efb547222df812f30
Vern Francisco <avernethy@gmail.com>
2018-08-25 16:05:27 -0500

    remove declaration

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -125,7 +125,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 
  cout << "Lambda: " << lambda_ << endl;
  //set vector for weights
- VectorXd weights_ = VectorXd(2 * n_aug_ + 1);
+ weights_ = VectorXd(2 * n_aug_ + 1);
  double weight_0 = lambda_/(lambda_ + n_aug_);
  weights_(0) = weight_0;
  for (int i=1; i<2*n_aug_ + 1; i++) {  
