83f01a78ffba7c1a5b11bd50ad5e1d20e806dfa9
Vern Francisco <avernethy@gmail.com>
2018-08-25 23:49:52 -0500

    adjust std_yawdd to 1

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -28,7 +28,7 @@ UKF::UKF() {
   std_a_ = 1; // <- MODIFY THIS
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 2; // <- MODIFY THIS
+  std_yawdd_ = 1; // <- MODIFY THIS
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
